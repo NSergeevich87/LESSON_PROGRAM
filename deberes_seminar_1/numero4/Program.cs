a26141555254f5470403a08151bf2ecb52eae0c5
NSergeevich <niknikitaser@gmail.com>
2023-02-24 15:39:26 +0100

    done numero 4

---
+++ deberes_seminar_1/numero4/Program.cs
@@ -0,0 +1,60 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход 
+// три числа и выдает максимальное из этих чисел.
+
+Console.Write("Vvedite pervoe chislo: ");
+int a = int.Parse(Console.ReadLine());
+
+Console.Write("Vvedite vtoroe chislo: ");
+int b = int.Parse(Console.ReadLine());
+
+Console.Write("Vvedite tretye chislo: ");
+int c = int.Parse(Console.ReadLine());
+
+int max = 0;
+
+//if (a > b)
+//{
+//    if (a > c)
+//    {
+//        max = a;
+//        Console.WriteLine($"max = {max}");
+//    }
+//    else
+//    {
+//    max = c;
+//    Console.WriteLine($"max = {max}"); 
+//    }
+//    
+//}
+//else
+//{
+//    if (b > c)
+//{
+//    max = b;
+//    Console.WriteLine($"max = {max}");
+//}
+//else
+//{
+//max = c;
+//Console.WriteLine($"max = {max}");
+//}
+//}
+
+if (a > b & a > c)
+{
+    max = a;
+    Console.WriteLine($"max = {max}");
+}
+else 
+{
+    if (b > c)
+    {
+    max = b;
+    Console.WriteLine($"max = {max}");  
+    }
+    else
+    {
+    max = c;
+    Console.WriteLine($"max = {max}");
+    }
+}
