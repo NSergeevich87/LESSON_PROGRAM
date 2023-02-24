790fe90f12dd3b7feee2fa000863ce94f716afa0
NSergeevich <niknikitaser@gmail.com>
2023-02-24 17:08:18 +0100

    done numero 8

---
+++ deberes_seminar_1/numero8/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N.
+
+Console.Write("Vvedite naturalnoe chislo: ");
+int N = int.Parse(Console.ReadLine());
+
+int count = 0;
+
+if (N < 0)
+{
+    while (count > N+1)
+   {
+    count -= 2;    
+    Console.Write($" {count}");
+   }
+}
+else
+{
+    while (count < N-1)
+   {
+    count += 2;    
+    Console.Write($" {count}");
+   }
+}
