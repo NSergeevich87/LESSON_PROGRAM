173b3d66b9c25d88114881e78876b84e9b3f684b
NSergeevich <niknikitaser@gmail.com>
2023-02-24 16:24:24 +0100

    done numero 6

--- deberes_seminar_1/numero6/Program.cs
+++ deberes_seminar_1/numero6/Program.cs
@@ -1,4 +1,14 @@
 ﻿// Задача 6: Напишите программу, которая на вход принимает число и выдает, 
 // является ли число четным (делится ли оно на два без остатка).
 
+Console.Write("Vvedite chislo y poluchite otvet delitsya li ono na 2: ");
+int N = int.Parse(Console.ReadLine());
 
+if (N % 2 == 0)
+{
+    Console.WriteLine("Chislo delitsya na 2, a znachit ono chetnoe");
+}
+else
+{
+    Console.WriteLine("Chislo ne delitsya na 2, a znachit ono NE chetnoe");
+}
