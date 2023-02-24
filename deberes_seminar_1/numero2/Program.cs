125f8c375131252d747aff98058e810a3ac3d085
NSergeevich <niknikitaser@gmail.com>
2023-02-24 15:04:51 +0100

    done numero 2

---
+++ deberes_seminar_1/numero2/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает 
+// два числа и выдает, какое число большее, а какое меньшее.
+
+Console.Write("Vvedite pervoe naturalnoe chislo: ");
+int x = int.Parse(Console.ReadLine());
+
+Console.Write("Vvedite vtoroe naturalnoe chislo: ");
+int y = int.Parse(Console.ReadLine());
+
+int max = 0;
+
+if (x > y)
+{
+    max = x;
+    Console.WriteLine($"{max} Bolshe");
+}
+else
+{
+    max = y;
+    Console.WriteLine($"{max} Bolshe");
+}
