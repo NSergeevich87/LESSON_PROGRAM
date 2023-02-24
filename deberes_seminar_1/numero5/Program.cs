4564d7ab23a621e94cc2a19c1e6497997b0ddf3a
NSergeevich <niknikitaser@gmail.com>
2023-02-24 14:31:53 +0100

    done numero 5

--- deberes_seminar_1/numero5/Program.cs
+++ deberes_seminar_1/numero5/Program.cs
@@ -3,5 +3,21 @@
 // 4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4" 
 // 2 -> " -2, -1, 0, 1, 2"
 
-
-
+Console.Write("vvedite celoe naturalnoe chislo: ");
+int N = int.Parse(Console.ReadLine());
+int Count = -N-1;
+int x = 0;
+if (N>0)
+{
+    while (Count < N)
+    {
+        x = Count + 1;
+        Console.Write($" {x}");
+        Count++;
+    }
+    //Console.WriteLine(-N);
+}
+else
+{
+    Console.WriteLine("nekorrektnoe chislo");
+}
