e30281e3d48970fbf226ab1ffb858eacb33156a7
NSergeevich <niknikitaser@gmail.com>
2023-02-28 16:52:54 +0100

    numero15

---
+++ deberes_seminar_2/numero15/Program.cs
@@ -0,0 +1,21 @@
+﻿
+// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+System.Console.Write("Vvedite cifru dnya nedely: ");
+int number = int.Parse(Console.ReadLine());
+
+if (number < 1 | number > 7)
+{
+    System.Console.WriteLine("V nashey vselennoy nedelya v diapazone 1 - 7");
+}
+
+if (number == 6 | number == 7)
+{
+    System.Console.WriteLine("Eto vijodnoy dyen!");
+}
+
+if (number > 0 & number < 6)
+{
+    System.Console.WriteLine("Rano rasslablyatsya, somoe vremya porabotat");
+}
+
