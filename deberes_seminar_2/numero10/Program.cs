3d2cecc429e012edeb6b7bfe03f38ac2766586b4
NSergeevich <niknikitaser@gmail.com>
2023-02-28 16:23:31 +0100

    numero13

--- deberes_seminar_2/numero10/Program.cs
+++ deberes_seminar_2/numero10/Program.cs
@@ -4,6 +4,11 @@
 System.Console.Write("Vvedite trej znachnoe chislo: ");
 int number = int.Parse(Console.ReadLine());
 
+if (number < 0)
+{
+    number = number * (-1);
+}
+
 if (number < 100 | number > 999)
 {
     System.Console.WriteLine("Eto ne trej znachnoe chislo!");
---
+++ deberes_seminar_2/numero13/Program.cs
@@ -0,0 +1,28 @@
+﻿
+// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, 
+// что третьей цифры нет.
+
+System.Console.Write("Vvedite chislo: ");
+int number = int.Parse(Console.ReadLine());
+
+int count = 0;
+
+if (number < 0)
+{
+    number = number * (-1);
+}
+
+if (number < 100)
+{
+    System.Console.WriteLine("Tretyey cifry net!");
+}
+
+if (number > 99)
+{
+while (number > 999)
+{
+    number = number / 10;
+}
+number %= 10;
+System.Console.WriteLine(number);
+}
