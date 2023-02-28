6a857c22c9a82b3ad6269aff85aee2b9978ac34d
NSergeevich <niknikitaser@gmail.com>
2023-02-28 15:16:34 +0100

    numero10

---
+++ deberes_seminar_2/numero10/Program.cs
@@ -0,0 +1,16 @@
+﻿
+// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+System.Console.Write("Vvedite trej znachnoe chislo: ");
+int number = int.Parse(Console.ReadLine());
+
+if (number < 100 | number > 999)
+{
+    System.Console.WriteLine("Eto ne trej znachnoe chislo!");
+}
+else
+{
+    number /= 10;
+    number %= 10;
+    System.Console.WriteLine(number);
+}
