7dc6d205abac58929abc59c14963596fa2e62a2f
NSergeevich <niknikitaser@gmail.com>
2023-03-03 20:37:41 +0100

    numero23

--- deberes_seminar_3/numero23/Program.cs
+++ deberes_seminar_3/numero23/Program.cs
@@ -4,3 +4,35 @@
 5 -> 1, 8, 27, 64, 125
 */
 
+double cubo(double arg)
+{
+    arg = Math.Abs(arg);
+    
+    double count = 0;
+    double x = 1;
+
+    while (x <= arg)
+    {
+
+        if (x == arg)
+        {
+            count = Math.Pow(x, 3);
+
+            System.Console.Write(count);
+
+            break;
+        }
+
+        count = Math.Pow(x, 3);
+
+        System.Console.Write($"{count}, ");
+
+        x++;
+    }
+    return count;
+}
+
+System.Console.Write("Vvedite chislo N: ");
+double n = double.Parse(Console.ReadLine());
+
+cubo(n);
