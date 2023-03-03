28d6078080c47d05c1390f9bdbbe6387e0b4a3eb
NSergeevich <niknikitaser@gmail.com>
2023-03-03 16:26:31 +0100

    numero21

--- deberes_seminar_3/numero21/Program.cs
+++ deberes_seminar_3/numero21/Program.cs
@@ -3,4 +3,36 @@
 на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
 A (3,6,8); B (2,1,-7), -> 15.84
 A (7,-5, 0); B (1,-1,9) -> 11.53
+Формула: AB = √((x2 - x1)2 + (y2 - y1)2 + (z2 - z1)2)
 */
+
+double ab3Dlength(double arg1, double arg2, double arg3, double arg4, double arg5, double arg6)
+{
+    double result = Math.Sqrt(Math.Pow(arg2 - arg1, 2) + Math.Pow(arg4 - arg3, 2) + Math.Pow(arg6 - arg5, 2));
+
+    return result;
+}
+
+System.Console.Write("x1: ");
+double x1 = double.Parse(Console.ReadLine());
+
+System.Console.Write("y1: ");
+double y1 = double.Parse(Console.ReadLine());
+
+System.Console.Write("z1: ");
+double z1 = double.Parse(Console.ReadLine());
+
+System.Console.Write("x2: ");
+double x2 = double.Parse(Console.ReadLine());
+
+System.Console.Write("y2: ");
+double y2 = double.Parse(Console.ReadLine());
+
+System.Console.Write("z2: ");
+double z2 = double.Parse(Console.ReadLine());
+
+double length = ab3Dlength(x1, x2, y1, y2, z1, z2);
+
+//ab3Dlength(x1, x2, y1, y2, z1, z2);
+
+System.Console.WriteLine("Rasstoyaniye: " + Math.Round(length, 2));
