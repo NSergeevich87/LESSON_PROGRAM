ffdfac9a09811619a45643e543be576aeaf93e82
NSergeevich <niknikitaser@gmail.com>
2023-03-17 14:22:42 +0100

    deberes7

--- deberes_seminar_7/numero47/Program.cs
+++ deberes_seminar_7/numero47/Program.cs
@@ -4,3 +4,53 @@ m = 3, n = 4.
 1 -3,3 8 -9,9
 8 7,8 -7,1 9 */
 
+int NewMessage(string message)
+{
+    System.Console.Write(message);
+    string answer = Console.ReadLine();
+    int numero = int.Parse(answer);
+    return numero;
+}
+
+/* double NewMessageD(string message)
+{
+    System.Console.Write(message);
+    string answer = Console.ReadLine();
+    double numero = double.Parse(answer);
+    return numero;
+} */
+
+double[,] Gen2DArray(int r, int c)
+{
+    double[,] array = new double[r, c];
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = Math.Round(new Random().NextDouble() * 100, 2);
+        }
+    }
+
+    return array;
+}
+
+void Print2DArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write($"{array[i, j]} ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int row = NewMessage("Введите количество строк в массиве: ");
+int column = NewMessage("Введите количество столбцов в массиве: ");
+// double min = NewMessageD("Введите диапазон заполнения массива ОТ: ");
+// double max = NewMessageD("Введите диапазон заполнения массива ДО: ");
+
+double[,] newArray = Gen2DArray(row, column/* , min, max */);
+Print2DArray(newArray);
--- deberes_seminar_7/numero50/Program.cs
+++ deberes_seminar_7/numero50/Program.cs
@@ -5,3 +5,70 @@
 8 4 2 4
 17 -> такого числа в массиве нет */
 
+int NewMessage(string message)
+{
+    System.Console.Write(message);
+    string answer = Console.ReadLine();
+    int numero = int.Parse(answer);
+    return numero;
+}
+
+int[,] Gen2DArray(int r, int c, int mi, int ma)
+{
+    int[,] array = new int[r, c];
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(mi, ma + 1);
+        }
+    }
+
+    return array;
+}
+
+void Print2DArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write($"{array[i, j]} ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+void FindElement(int[,] array, int r, int c)
+{
+    if (r <= array.GetLength(0) && c <= array.GetLength(1) && r > 0 && c > 0)
+    {
+    for (int i = r - 1; i < array.GetLength(0); i++)
+    {
+        for (int j = c - 1; j < array.GetLength(1); j++)
+        {
+            Console.WriteLine($"Элемент поиска равен: {array[i,j]}");
+            break;
+        }
+        break;
+    }
+    }
+    else
+    {
+        Console.WriteLine("Элемент не может быть найден т.к. он находится вне заданного диапазона массива!");
+    }
+}
+
+int row = NewMessage("Введите количество строк в массиве: ");
+int column = NewMessage("Введите количество столбцов в массиве: ");
+int min = NewMessage("Введите диапазон заполнения массива ОТ: ");
+int max = NewMessage("Введите диапазон заполнения массива ДО: ");
+
+int[,] newArray = Gen2DArray(row, column, min, max);
+Print2DArray(newArray);
+
+int findRowElem = NewMessage("Введите НОМЕР! строки для поиска элемента: ");
+int findColElem = NewMessage("Введите НОМЕР! столбца для поиска элемента: ");
+
+FindElement(newArray, findRowElem, findColElem);
