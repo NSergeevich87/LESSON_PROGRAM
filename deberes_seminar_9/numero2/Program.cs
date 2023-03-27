f34bd4d965d116b71b4a21c8b1a84331b3ed8ef7
NSergeevich <niknikitaser@gmail.com>
2023-03-27 13:07:07 +0200

    fix_2

--- deberes_seminar_9/numero2/Program.cs
+++ deberes_seminar_9/numero2/Program.cs
@@ -4,39 +4,27 @@ M = 4; N = 8. -> 30 */
 
 System.Console.Clear();
 
-ulong NuevoMensaje(string mensaje)
+int NuevoMensaje(string mensaje)
 {
     System.Console.Write(mensaje);
     string respuesta = System.Console.ReadLine();
-    ulong numero = ulong.Parse(respuesta);
+    int numero = int.Parse(respuesta);
     return numero;
 }
 
-void IntervaloNumerosPares(ulong de, ulong a, ulong sum = 0)
+int IntervaloNumerosPares(int de, int a, int sum = 0)
 {
-    ulong temp = 0;
-
     if (de > a)
     {
-        temp = de;
-        de = a;
-        a = temp;
+        return sum;
     }
-
-    if (de == a)
-    {
-        sum = sum + de;
-        System.Console.WriteLine($"В заданном диапазоне, сумма натуральных элементов равна: {sum}");
-        return;
-    }
-    else
-    {
-        sum = sum + de;
-        IntervaloNumerosPares(de + 1, a, sum);
-    }
+    
+    sum = sum + de;
+    return IntervaloNumerosPares(de + 1, a, sum);
 }
 
-ulong desdeNumero = NuevoMensaje("Введите диапазон ОТ: ");
-ulong hastaNumero = NuevoMensaje("Введите диапазон ДО: ");
+int desdeNumero = NuevoMensaje("Введите диапазон ОТ: ");
+int hastaNumero = NuevoMensaje("Введите диапазон ДО: ");
 
-IntervaloNumerosPares(desdeNumero, hastaNumero);
+int numberSum = IntervaloNumerosPares(desdeNumero, hastaNumero);
+System.Console.WriteLine($"В заданном диапазоне, сумма натуральных элементов равна: {numberSum}");
